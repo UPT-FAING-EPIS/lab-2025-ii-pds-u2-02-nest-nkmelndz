ae8ded49698cc26a617b1d92c788d5b0f76dcb9d
nkmelndz <nkmelndz@users.noreply.github.com>
2025-11-02 04:46:42 +0000

    deploy: c157f85235676faedc7f9d89bbd0f190698865da

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -15,7 +15,7 @@ customElements.define('compodoc-menu', class extends HTMLElement {
         <nav>
             <ul class="list">
                 <li class="title">
-                    <a href="index.html" data-type="index-link">customer-app documentation</a>
+                    <a href="index.html" data-type="index-link">@UPT-FAING-EPIS/customer-app documentation</a>
                 </li>
 
                 <li class="divider"></li>
